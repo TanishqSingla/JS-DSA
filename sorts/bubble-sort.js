866383842514d42f1a2f66c30b0729a7f9879cfe
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-30 18:10:23 +0530

    Added optimized bubblesort

--- sorts/bubble-sort.js
+++ sorts/bubble-sort.js
@@ -1,17 +1,16 @@
-const assert = require('assert');
-
 function bubbleSort(arr) {
-    let len = arr.length;
-
-    for(let i = 0; i < len - 1; i++) {
-        for(let j = 0; j < len - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+   let swapped = false; 
+    do {
+        swapped = false;
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] > arr[i + 1]) {
+                const temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+                swapped = true;
             }
         }
-    }
+    }while(swapped);
 
     return arr;
 }
